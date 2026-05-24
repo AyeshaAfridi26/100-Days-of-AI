b45877dedb4d65ba5b394ee54b8ead2b6abb84b8
unknown <khanafridiayesha@gmail.com>
2026-05-24 12:51:54 +0500

    Initial commit: 100 Days of AI learning journey

---
+++ Python/04_virtual_env.py
@@ -0,0 +1,22 @@
+# USING PYTHON
+''' 
+1. # Navigate to your project folder
+cd path/to/your/project
+2. # Create the environment (creates a folder named .venv)
+python -m venv .venv
+3. # Activate it
+# On Windows (Command Prompt):
+.venv\Scripts\activate
+# On Mac/Linux (or Windows Git Bash):
+source .venv/bin/activate
+'''
+
+# USING CONDA
+'''
+ Env is necessary for using multiple libraries and modules
+1. cmd prmpt ---> conda create -p venv python =3.12
+2. activation ---> conda activate venv
+3. deactivation ---> conda deactivate 
+4. Delete it
+# Simply right-click the ".venv" folder in VS Code and hit Delete.
+'''
